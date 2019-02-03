c9be24a4806508da873edd89116d2a96c6643136
smallhorse87 <smallhorse87@163.com>
2019-02-03 16:57:51 +0800

    UITableView+Extension done

--- Common/Extension/UITableView+Extension.swift
+++ Common/Extension/UITableView+Extension.swift
@@ -16,6 +16,39 @@ extension String {
     }
 }
 
-class UITableView_Extension: NSObject {
+/**
+ 向tableView 注册UITableViewCell
+ 
+ - parameter tableView: tableView
+ - parameter cell: 要注册的类名
+ */
+
+func regClass (_ tableView:UITableView, cell:AnyClass)->Void {
+    tableView.register(cell, forCellReuseIdentifier: "\(cell)")
+}
+
+/**
+ 从tableView缓存中取出对应类型的Cell
+ 
+ - parameter tableView: tableView
+ - parameter cell:  要返回的Cell类型
+ - parameter indexPath: 位置
+ */
+func getCell<T: UITableViewCell> (_ tableView:UITableView, cell: T.Type, indexPath:IndexPath) -> T
+{
+    return tableView.dequeueReusableCell(withIdentifier: "\(cell)", for: indexPath) as! T
+}
+
+extension UITableView {
+    func v2_scrolleToBottom() {
+        let section = self.numberOfSections - 1
+        let row = self.numberOfRows(inSection: section) - 1
+        if section < 0 || row < 0 {
+            return
+        }
+        
+        let path = IndexPath(row: row, section: section)
+        self.scrollToRow(at: path, at: .top, animated: false)
+    }
 
 }
