274d7b7ac7bec540278b4b72ed265b9e271807aa
chenxiaosong <smallhorse87@163.com>
2019-02-11 14:18:38 +0800

    AppDelegate Done

--- swift_exercise/AppDelegate.swift
+++ swift_exercise/AppDelegate.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+
+import DrawerController
+import SVProgressHUD
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +21,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        URLProtocol.registerClass(WebViewImageProtocol.self)
+        
+        self.window = UIWindow()
+        self.window?.frame = UIScreen.main.bounds
+        self.window?.makeKeyAndVisible()
+        
+        let centerNav = V2EXNavigationController(rootViewController: HomeViewController())
+        let leftViewController = HomeViewController();//LeftViewController()
+        let rightViewController = HomeViewController();//RightViewController()
+        let drawerController = DrawerController(centerViewController: centerNav, leftDrawerViewController: centerNav, rightDrawerViewController:centerNav)
+        
+        self.window?.themeChnagedHandler = { [weak self] (style) -> Void in
+            self?.window?.backgroundColor = V2EXColor.colors.v2_backgroundColor
+        }
+        
+        drawerController.maximumLeftDrawerWidth=230
+//        drawerController.maximumRightDrawerWidth = rightViewController.maxim
+        drawerController.openDrawerGestureModeMask = OpenDrawerGestureMode.panningCenterView
+        drawerController.closeDrawerGestureModeMask = CloseDrawerGestureMode.all
+        self.window?.rootViewController = drawerController
+        
+        V2Client.sharedInstance.drawerController = drawerController
+        V2Client.sharedInstance.centerViewController = centerNav.viewControllers[0] as? HomeViewController
+        V2Client.sharedInstance.centerNavigation = centerNav
+        
+        #if DEBUG
+            let fpsLabel = V2FPSLabel(frame: CGRect(x: 15, y: SCREEN_HEIGHT-40, width: 55, height: 20))
+            self.window?.addSubview(fpsLabel)
+        #else
+        #endif
+        
+        SVProgressHUD.setForegroundColor(UIColor(white: 1, alpha: 1))
+        SVProgressHUD.setBackgroundColor(UIColor(white: 0.15, alpha: 0.85))
+        SVProgressHUD.setDefaultMaskType(.none)
+        SVProgressHUD.setMinimumDismissTimeInterval(1.5)
+        SVProgressHUD.setContainerView(self.window!)
+        
+        #if DEBUG
+        #else
+        Fabric.with([Crashlytics.self])
+        #endif
+        
         return true
     }
 
@@ -33,10 +81,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
+    fileprivate var lastPasteString: String?
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if let pasteString = UIPasteboard.general.string {
+            guard lastPasteString != pasteString else {
+                return
+            }
+            self.lastPasteString = pasteString
+            
+            let result = AnalyzURLResultType(url: pasteString)
+            switch result {
+            case .member(let member):
+                let controller = UIAlertController(title: "是否打开用户主页？", message: pasteString, preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "打开", style: .default, handler: {(_) in
+                    member.run()
+                }))
+                controller.addAction(UIAlertAction(title: "忽略", style: .cancel, handler: nil))
+                V2Client.sharedInstance.centerNavigation?.present(controller, animated: true, completion: nil)
+            
+            case .topic(let topic):
+                let controller = UIAlertController(title: "是否打开帖子？", message: pasteString, preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "打开", style: .default, handler: {(_) in
+                    topic.run()
+                }))
+                controller.addAction(UIAlertAction(title: "忽略", style: .cancel, handler: nil))
+                V2Client.sharedInstance.centerNavigation?.present(controller, animated: true, completion: nil)
+                
+            default:
+                return
+            }
+        }
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
